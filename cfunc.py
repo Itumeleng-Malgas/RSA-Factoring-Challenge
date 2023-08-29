7abf26cac3c7ee2828c463ceb73ba4328b73ef36
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-08-29 17:19:49 +0200

    factors

--- cfunc.py
+++
@@ -1,8 +0,0 @@
-import ctypes
-import os
-
-path = os.getcwd()
-clibrary = ctypes.CDLL(os.path.join(path, 'libfactorize.so'))
-
-clibrary.factorize.restype = ctypes.POINTER(ctypes.c_uint64)
-clibrary.factorize.argtypes = [ctypes.c_uint64]
