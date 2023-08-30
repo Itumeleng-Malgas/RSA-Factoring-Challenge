808cd4cc68272ad42fddf4a3b99debdea79e7fd2
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-08-30 10:30:05 +0200

    rsa_revised

--- factorize.c
+++ factorize.c
@@ -1,26 +1,32 @@
-#include <stdlib.h>
-#include <stdint.h>
 #include <stdio.h>
-#include <math.h>
 
-uint64_t *factorize(uint64_t N) {
-    uint64_t sqrt_N = (uint64_t)sqrt(N);
-    
-    uint64_t *factors = (uint64_t *)malloc(sizeof(uint64_t) * 2);
-    if (!factors) {
-        perror("Memory allocation error");
-        return (NULL);
-    }
-    int count = 0;
+/*
+ * find_factors - find the smallest divisor N.
+ * @N: given integer.
+ * Return: 0 if the number is prime.
+ */
+int find_factors(long int N)
+{
+	long int factor;
 
-    for (uint64_t factor = 1; factor <= sqrt_N; ++factor) {
-        if (N % factor == 0 && !(factor == N || factor == 1)) {
-            factors[count++] = factor;
-            if (factor != N / factor) {
-                factors[count++] = N / factor;
-            }
-        }
-    }
+	if (N % 2 == 0)
+	{
+		printf("%lu=%lu*%i\n", N, N / 2, 2);
+		return (0);
+	}
 
-    return factors;
+	factor = 3;
+	while (factor * factor <= N)
+	{
+		if (N % factor == 0)
+		{
+			printf("%lu=%lu*%lu\n", N, N / factor, factor);
+			return (0);
+		}
+		else
+			factor += 2;
+	}
+
+	printf("%lu=%lu*%i\n", N, N, 1);
+	return (0);
 }
