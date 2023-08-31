fecb0814ff00e99709ff1c35d06e8bc14acfc072
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-08-31 10:40:20 +0200

    rsa_primes

--- rsa.py
+++ rsa.py
@@ -11,9 +11,18 @@ prime_factors_lib.find_prime_factors.argtypes = [ctypes.c_char_p]
 prime_factors_lib.find_prime_factors.restype = None
 
 def find_prime_factors():
-    with open(sys.argv[1]) as f:
-        num_str = f.readline().strip()
-        prime_factors_lib.find_prime_factors(num_str.encode('utf-8'))
-            
+     
+    if len(sys.argv) != 2:
+        print("Usage: factors <file>")
+        exit()
+
+    try:
+        with open(sys.argv[1], 'r') as f:
+            num_str = f.readline().strip()
+            prime_factors_lib.find_prime_factors(num_str.encode('utf-8'))
+    except FileNotFoundError:
+        print("File not found")
+        exit()
+        
 if __name__ == '__main__':
     find_prime_factors()
